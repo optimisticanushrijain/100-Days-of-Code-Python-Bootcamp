5b1ea49eaca75b6705dc72b1f4dba3c7968d436b
anushri <anushrijainpngr@gmail.com>
2025-10-24 18:07:25 -0500

    Day1 changes

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,17 +1,4 @@
-# Write your code below this line 👇
-# print("Hello world!")
-# print("Hello world!")
-# print("Hello world!")
-
-# Using \n - to add another line or new line escape sequence
-print("Hello world!\nHello world!\nHello world!\n")
-
-# String Concatenation using +
-print("Hello" + "Anushri")
-print("Hello" + "" + "Anushri\n")
-
-# Add a space between the two strings
-print("Hello" + " " + "Anushri")
-print("Hello " + "Anushri")
-print("Hello" + " Anushri")
-
+print("Welcome to the Band Name Generator.")
+user_city_name = input("What's the name of the city you grew up in?\n")
+user_pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + user_city_name + " " + user_pet_name)
