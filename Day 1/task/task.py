b7ca9d2fb169ab6ca4bc2694db07a8a0d6a4bce0
anushri <anushrijainpngr@gmail.com>
2025-10-24 00:27:55 -0500

    Added day 1 solution and Think Python pdf

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,5 +1,17 @@
- # This line of code will take an input using the input() function
- # Comment- # + /
-#print("Hello "+ input("Name") + "!")
-input(1234)
+# Write your code below this line 👇
+# print("Hello world!")
+# print("Hello world!")
+# print("Hello world!")
+
+# Using \n - to add another line or new line escape sequence
+print("Hello world!\nHello world!\nHello world!\n")
+
+# String Concatenation using +
+print("Hello" + "Anushri")
+print("Hello" + "" + "Anushri\n")
+
+# Add a space between the two strings
+print("Hello" + " " + "Anushri")
+print("Hello " + "Anushri")
+print("Hello" + " Anushri")
 
